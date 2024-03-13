4cc13b2c4fbb8a07bafa285142a41e05eac20e16
4am_walking <chanman0808@gmail.com>
2024-03-13 13:32:53 -0400

    initial commit of login api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import morgan from 'morgan';
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const port = process.env.PORT || 8080;
 const cors = require('cors');
 const authRoutes = require('./routes/auth.routes');
 
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(authRoutes);
