f5a3fccf11d066b2ae352446a4cbeefe09fa6dc2
4am_walking <chanman0808@gmail.com>
2024-03-15 15:50:58 -0400

    initial commit of auth ui

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Layout from "../layout/Layout";
+
+const Home = () => {
+    return (
+        <Layout>
+            <p>Nothing to see here yet</p>
+        </Layout>
+    )
+}
+
+export default Home;
